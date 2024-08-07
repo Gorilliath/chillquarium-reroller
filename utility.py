0df777190517676e9f796c0f1491fee8bb312e46
Gorilliath <gorilliath59@gmail.com>
2024-08-07 23:35:52 +0100

    Confine `pyautogui` locate methods and `utility.getCentre` to the region of the game window
    Will default to screen if specific window is not found

--- utility.py
+++ utility.py
@@ -1,4 +1,5 @@
 import pyautogui
+import pygetwindow
 
 from config import LOCATE_ARGS
 
@@ -18,14 +19,38 @@ def print_help():
     print("---")
 
 
+def get_visible_window_with_title(title):
+    try:
+        windows = pygetwindow.getWindowsWithTitle(title)
+        for window in windows:
+            if window.visible and window.title == title:
+                return window
+    except:
+        return
+
+
+def get_window_position_and_size_as_region():
+    try:
+        window = get_visible_window_with_title("Chillquarium")
+        if window:
+            return (window.left, window.top, window.width, window.height)
+    except:
+        print("... Failed to get region from game window: defaulting to screen region")
+    return (0, 0, *pyautogui.size())
+
+
+def get_locate_args():
+    return {**LOCATE_ARGS, "region": get_window_position_and_size_as_region()}
+
+
 def get_centre_point():
-    screen_width, screen_height = pyautogui.size()
-    return [screen_width / 2, screen_height / 2]
+    left, top, width, height = get_window_position_and_size_as_region()
+    return [left + (width / 2), top + (height / 2)]
 
 
 def image_exists(image_path):
     try:
-        img = pyautogui.locateOnScreen(image_path, **LOCATE_ARGS)
+        img = pyautogui.locateOnScreen(image_path, **get_locate_args())
         if img:
             return True
     except:
@@ -34,7 +59,7 @@ def image_exists(image_path):
 
 def try_click_image(image_path):
     try:
-        img = pyautogui.locateOnScreen(image_path, **LOCATE_ARGS)
+        img = pyautogui.locateOnScreen(image_path, **get_locate_args())
         if img:
             pyautogui.click(img)
     except:
@@ -43,7 +68,9 @@ def try_click_image(image_path):
 
 def try_click_nth_image(image_path, index):
     try:
-        image_positions = list(pyautogui.locateAllOnScreen(image_path, **LOCATE_ARGS))
+        image_positions = list(
+            pyautogui.locateAllOnScreen(image_path, **get_locate_args())
+        )
         image = image_positions[index]
         if image:
             pyautogui.click(image)
