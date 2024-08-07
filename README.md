5939b4d9acc82a6aa0994aba08a05a420a544c3b
Gorilliath <gorilliath59@gmail.com>
2024-08-07 21:06:14 +0100

    Allowed main logic to loop indefinitely and still be start/stopped
    Main logic execution moved to its own thread in order to not block listening for further commands

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 ## Prerequisites
 
+### To run this tool
+
 - Python
 - `pip install -r requirements.txt`
 
-## Main logic loop
+### Game settings/state
 
-- Ensure any open popup window is closed
+- Ensure 'Fewer clicks' option is enabled so this tool doesn't need to hold LMB when adding fish to the tank
+- Ensure your game's 'Color' filter is enabled so golden and rainbow fish are at the top of the tank
+- Ensure you have a tank with 'Gilded' in its name to receive the golden and rainbow fish
+
+## What this tool's main logic loop will do
+
+- Ensure any open game popup window is closed
 - Open the shop
-- Buy max amount of fish (Which fish depends on which key you press)
+- Buy max amount of fish (Which fish depends on which key you have last pressed to configure that)
 - Open fish packs
 - Add fish to tank
 - Open current tank
@@ -16,12 +24,12 @@
 
 ## Usage
 
-- Ensure your game's 'Color' filter is on so golden and rainbow fish are at the top of the tank
 - Start the program in a terminal with `py main.py`
-- The program will continuously listen for the following keys and, once pressed, will execute the main logic accordingly one time:
-  - F1: Freshwater Friends
-  - F2: Rivers and Ponds
-  - F3: Reef Fellas
-  - F4: Marine Dwellers
-  - F5: Giants
-  - F6: Spring Pals
+- Configure the fish that will be bought (Can be done at any time):
+  - `F1`: Freshwater Friends
+  - `F2`: Rivers and Ponds
+  - `F3`: Reef Fellas
+  - `F4`: Marine Dwellers
+  - `F5`: Giants
+  - `F6`: Spring Pals
+- Press `F11` to start/stop the execution of the main logic (When you stop it, it will finish the current iteration and stop gracefully)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import keyboard
 import pyautogui
+import threading
 import time
 from enum import Enum
 
@@ -29,7 +30,11 @@ def print_help():
     print("F4: Marine Dwellers")
     print("F5: Giants")
     print("F6: Spring Pals")
+    print("---")
+    print("F11: Start/Stop")
+    print("---")
     print("Listening for keypresses...")
+    print("---")
 
 
 def get_centre_point():
@@ -62,7 +67,7 @@ def try_click_nth_image(image_path, index):
         if image:
             pyautogui.click(image)
     except:
-        print("f...Failed to click nth: {index} something - continuing anyway...")
+        print(f"...Failed to click nth: {index} something - continuing anyway...")
 
 
 # Atomic game interactions
@@ -124,74 +129,81 @@ def sell_all_unlocked_fish():
     try_click_image("img/2560-1440-sell-all-unlocked-fish-button.png")
 
 
-# Main logic
+# Main thread state
+
+shop_item_to_buy = ShopItems.FRESHWATER_FRIENDS
+running = False
+main_thread = None
 
 
-def main(shop_item):
-    try:
-        print("Closing any popup windows...")
-        close_any_popup_windows()
-        time.sleep(1)
-        print("Opening shop...")
-        open_shop()
-        time.sleep(1)
-        print(f"Buying fish packs ({shop_item})")
-        buy_max_fish(shop_item)
-        time.sleep(1)
-        print("Closing shop...")
-        close_any_popup_windows()
-        print("Opening packs of fish...")
-        open_all_packs()
-        print("Adding fish to tank...")
-        put_fish_into_tank()
-        print("Opening current tank...")
-        open_current_tank()
-        time.sleep(1)
-        print("Filtering golden and rainbow to other tank...")
-        filter_golden_and_rainbow_to_other_tank()
-        print("Selling all unlocked fish...")
-        sell_all_unlocked_fish()
-        pyautogui.moveTo(get_centre_point())
-        time.sleep(1)
-    except:
-        print("... Main logic failed")
-    print_help()
+def change_shop_item(shop_item):
+    global shop_item_to_buy
+    shop_item_to_buy = shop_item
+    print(f"Shop item to buy set to: {shop_item_to_buy}")
 
 
-# Keyboard event functions
+def change_running_state(next_state):
+    global running, main_thread
+
+    running = next_state
+
+    if running and main_thread == None:
+        print("Starting thread with main logic...")
+        main_thread = threading.Thread(target=main)
+        main_thread.start()
+    elif running and main_thread:
+        print("Thread with main logic already running...")
+    elif not running and main_thread == None:
+        print("No thread found to close...")
+    elif not running and main_thread:
+        print(
+            "Thread with main logic will stop gracefully after it finishes its current iteration..."
+        )
 
 
-def freshwater_friends(event):
-    main(ShopItems.FRESHWATER_FRIENDS)
-
-
-def rivers_and_ponds(event):
-    main(ShopItems.RIVERS_AND_PONDS)
-
-
-def reef_fellas(event):
-    main(ShopItems.REEF_FELLAS)
-
-
-def marine_dwellers(event):
-    main(ShopItems.MARINE_DWELLERS)
-
-
-def giants(event):
-    main(ShopItems.GIANTS)
-
-
-def spring_pals(event):
-    main(ShopItems.SPRING_PALS)
+def main():
+    global main_thread
+    while running:
+        try:
+            print("Closing any popup windows...")
+            close_any_popup_windows()
+            time.sleep(1)
+            print("Opening shop...")
+            open_shop()
+            time.sleep(1)
+            print(f"Buying fish packs ({shop_item_to_buy})")
+            buy_max_fish(shop_item_to_buy)
+            time.sleep(1)
+            print("Closing shop...")
+            close_any_popup_windows()
+            print("Opening packs of fish...")
+            open_all_packs()
+            print("Adding fish to tank...")
+            put_fish_into_tank()
+            print("Opening current tank...")
+            open_current_tank()
+            time.sleep(1)
+            print("Filtering golden and rainbow to other tank...")
+            filter_golden_and_rainbow_to_other_tank()
+            print("Selling all unlocked fish...")
+            sell_all_unlocked_fish()
+            pyautogui.moveTo(get_centre_point())
+            time.sleep(1)
+        except:
+            print("...Main logic failed")
+        print_help()
+    main_thread = None
+    print("... Main thread stopped gracefully")
 
 
 # Registering keyboard events
-keyboard.on_press_key("F1", freshwater_friends)
-keyboard.on_press_key("F2", rivers_and_ponds)
-keyboard.on_press_key("F3", reef_fellas)
-keyboard.on_press_key("F4", marine_dwellers)
-keyboard.on_press_key("F5", giants)
-keyboard.on_press_key("F6", spring_pals)
+keyboard.on_press_key("F1", lambda e: change_shop_item(ShopItems.FRESHWATER_FRIENDS))
+keyboard.on_press_key("F2", lambda e: change_shop_item(ShopItems.RIVERS_AND_PONDS))
+keyboard.on_press_key("F3", lambda e: change_shop_item(ShopItems.REEF_FELLAS))
+keyboard.on_press_key("F4", lambda e: change_shop_item(ShopItems.MARINE_DWELLERS))
+keyboard.on_press_key("F5", lambda e: change_shop_item(ShopItems.GIANTS))
+keyboard.on_press_key("F6", lambda e: change_shop_item(ShopItems.SPRING_PALS))
+keyboard.on_press_key("F11", lambda e: change_running_state(not running))
 
 print_help()
 
